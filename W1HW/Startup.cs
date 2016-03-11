1927ec14f444df50f9b2a1e0b2ab864c39d36117
黃大都 <chingchou@gmail.com>
2016-03-11 23:04:52 +0800

    ver1

---
+++ W1HW/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(W1HW.Startup))]
+namespace W1HW
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
